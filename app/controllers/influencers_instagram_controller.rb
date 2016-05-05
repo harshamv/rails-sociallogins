fc3aa1e530c2d6b9affa792322a9a05cb80c878e
Harsha MV <harsha@mink7.com>
2016-05-05 14:06:56 +0530

    Instagram Code

--- app/controllers/influencers_instagram_controller.rb
+++ app/controllers/influencers_instagram_controller.rb
@@ -2,17 +2,34 @@ class InfluencersInstagramController < Api::V1::ApiController
   require "instagram"
 
   def create
-    @influencer = Influencer.new(first_name: instagram_client.full_name,
-                                 bio: instagram_client.bio,
-                                 profile_picture_url: instagram_client.profile_picture)
 
-    @influencer.influencer_authorizations.build(
-                    provider: 'instagram',
-                    provider_uid: instagram_client.id,
-                    oauth_token: session[:access_token])
+    Instagram.configure do |config|
+      config.client_id = "692379b5fd484fd393ca36ef76e917fc"
+      config.client_secret = "66991d56dea4481487e2542efec4c455"
+    end
 
-    @influencer.save()
-    render plain: @influencer.inspect
+    puts "======================================================"
+    p params.inspect
+    puts "======================================================"
+
+    response = Instagram.get_access_token(params[:code], :redirect_uri => call_back_url)
+
+    puts "======================================================"
+    p response[:access_token].inspect
+    puts "======================================================"
+
+
+    # @influencer = Influencer.new(first_name: instagram_client.full_name,
+    #                              bio: instagram_client.bio,
+    #                              profile_picture_url: instagram_client.profile_picture)
+    #
+    # @influencer.influencer_authorizations.build(
+    #                 provider: 'instagram',
+    #                 provider_uid: instagram_client.id,
+    #                 oauth_token: session[:access_token])
+    #
+    # @influencer.save()
+    render json: response
   end
 
 
@@ -20,16 +37,21 @@ class InfluencersInstagramController < Api::V1::ApiController
 
 
   def instagram_client
-    
+
     Instagram.configure do |config|
-      config.client_id = "979d4e4e56e843a696090b104224163f"
-      config.client_secret = "ced0f812bd454381b060337f4b3cd8ed"
+      config.client_id = "692379b5fd484fd393ca36ef76e917fc"
+      config.client_secret = "66991d56dea4481487e2542efec4c455"
     end
 
-    response = Instagram.get_access_token(params[:code], :redirect_uri => call_back_url)
-    session[:access_token] = response.access_token
-    client = Instagram.client(:access_token => session[:access_token])
-    user = client.user
+    puts params.inspect
+    #
+    # response = Instagram.get_access_token(params[:code], :redirect_uri => call_back_url)
+    #
+    # puts "======================================================"
+    # puts response.inspect
+    # session[:access_token] = response.access_token
+    # client = Instagram.client(:access_token => session[:access_token])
+    # user = client.user
   end
 
   def call_back_url
